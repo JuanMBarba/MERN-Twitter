88c37888309bf0f3e1838cad5b6f4c0acbdb9751
Juan Barba <barbajuan11@gmail.com>
2021-06-12 17:59:24 -0700

    Finish up to passport

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose')
 const express = require("express");
+const db = require('./config/keys').mongoURI
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const User = require("./models/User")
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World Again"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    })
+    user.save()
+    res.send("Hello World Again")
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-const db = require('./config/keys').mongoURI
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 mongoose
     .connect(db, { useNewUrlParser: true })
